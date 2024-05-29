ff0993233f6638118020c879de55afed58ab4072
nickjun12 <juniornicko6@gmail.com>
2024-05-29 09:43:04 +0700

    membuat website kopi senja gurau

---
+++ JS/script.js
@@ -0,0 +1,15 @@
+// TOOGLE CLASS ACTIVE
+const navbarNav = document.querySelector(".navbar-nav");
+// KETIKA HAMBURGER MENU DI KLIK
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// KLIK DILUAR SIDEBAR UNTUK MENGHILANGKAN NAV
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
